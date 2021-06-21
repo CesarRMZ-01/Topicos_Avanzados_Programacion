73a2423812a9d1bef5bcd0754a3c4fcb95f8bc98
CesarRMZ-01 <alejandrores4@hotmail.com>
2021-06-21 02:16:28 -0500

    Vistas-Cita CreacionVista-Presion

--- Presión arterial/Presión arterial.Android/MainActivity.cs
+++ Presión arterial/Presión arterial.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Presión_arterial.Droid
 {
-    [Activity(Label = "Presión_arterial", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ICare", Icon = "@drawable/Icono", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
